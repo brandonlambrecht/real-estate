1cfb119f6035dc777984880c5eee04dcc3c7bf09
Brandon Lambrecht <brandonjlambrecht@gmail.com>
2024-06-12 19:39:57 -0500

    create middleware and function to handle errors

--- api/index.js
+++ api/index.js
@@ -27,3 +27,14 @@ app.use("/api/user", userRouter);
 
 //Auth Route
 app.use("/api/auth", authRouter);
+
+//Middleware //
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Sever Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
