562d9bcd65584585a0c33c98ad77154f6ccd4517
Brandon Lambrecht <brandonjlambrecht@gmail.com>
2024-06-24 14:48:32 -0500

    add redux toolkit

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   })
   .catch((error) => {
     console.log(error);
@@ -17,21 +17,21 @@ mongoose
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
 
 app.use(express.json());
 
 //User Route
-app.use("/api/user", userRouter);
+app.use('/api/user', userRouter);
 
 //Auth Route
-app.use("/api/auth", authRouter);
+app.use('/api/auth', authRouter);
 
 //Middleware //
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
-  const message = err.message || "Internal Sever Error";
+  const message = error.message || 'Internal Sever Error';
   return res.status(statusCode).json({
     success: false,
     statusCode,
