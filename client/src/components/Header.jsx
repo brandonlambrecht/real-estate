4514932ec881e467034e7ec1cbbe650f2c1cb2f5
Brandon Lambrecht <brandonjlambrecht@gmail.com>
2024-06-24 23:28:29 -0500

    update header and make profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,30 +1,30 @@
-import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <header className="bg-neutral-400 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-zinc-900">Lambrecht</span>
-            <span className="text-stone-200">Estate</span>
+    <header className='bg-neutral-400 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-zinc-900'>Lambrecht</span>
+            <span className='text-stone-200'>Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
-          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
-          <FaSearch className="text-slate-500" />
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+          <FaSearch className='text-slate-500' />
         </form>
-        <ul className="flex gap-4">
-          <Link to="/">
-            <li className="hidden sm:inline text-stone-200 hover:underline">Home</li>
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-stone-200 hover:underline'>Home</li>
           </Link>
-          <Link to="/about">
-            <li className="hidden sm:inline text-stone-200 hover:underline">About</li>
+          <Link to='/about'>
+            <li className='hidden sm:inline text-stone-200 hover:underline'>About</li>
           </Link>
-          <Link to="/sign-in">
-            <li className="text-stone-200 hover:underline">Sign In</li>
-          </Link>
+          <Link to='/profile'>{currentUser ? <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' /> : <li className='text-stone-200 hover:underline'>Sign In</li>}</Link>
         </ul>
       </div>
     </header>
